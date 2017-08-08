4d2f32b03ff4711fc61683471182eea78cc7f277
James Han <chengchouhan@huskers.unl.edu>
2017-08-07 21:20:28 -0500

    Add files via upload

---
+++ private.py
@@ -0,0 +1,6 @@
+Consumer_Key = ""
+Consumer_Secret = ""
+Access_Token = ""
+Access_Token_Secret = ""
+
+engine = create_engine('postgresql://your_own_username:your_own_password@localhost:5432/your_db_name')
---
+++ settings.py
@@ -0,0 +1,9 @@
+TRACK_TERMS = ["Gary vee", "GaryVee", "Gary Vaynerchuk", "vaynermedia"]
+CONNECTION_STRING = ""
+CSV_NAME = "gary.csv"
+TABLE_NAME = "gary"
+
+try:
+    from private import *
+except Exception:
+    pass
