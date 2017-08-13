01d2d4ffc1ae118655b44fc24a6375a3c4c2e5dc
James Han <chengchouhan@huskers.unl.edu>
2017-08-13 15:00:20 -0500

    Update private.py

--- private.py
+++ private.py
@@ -3,4 +3,4 @@ Consumer_Secret = ""
 Access_Token = ""
 Access_Token_Secret = ""
 
-engine = create_engine('postgresql://your_own_username:your_own_password@localhost:5432/your_db_name')
+CONNECTION_STRING = "postgresql://your_own_username:your_own_password@localhost:5432/your_db_name"
