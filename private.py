db6296d8e171e5d7c9f908913e4b641bae6fd18b
James Han <chengchouhan@huskers.unl.edu>
2017-08-14 15:46:49 -0500

    Update private.py

--- private.py
+++ private.py
@@ -5,4 +5,8 @@ Consumer_Secret = ""
 Access_Token = ""
 Access_Token_Secret = ""
 
-CONNECTION_STRING = "postgresql://your_own_username:your_own_password@localhost:5432/your_db_name"
+user_name = ""
+password = ""
+DB_NAME = ""
+
+CONNECTION_STRING = "postgresql://" + user_name + ":" + password + "@localhost:5432/" + DB_NAME
