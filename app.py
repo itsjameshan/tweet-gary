696ce66804301d6314e288702a87f3ce12206861
James Han <chengchouhan@huskers.unl.edu>
2017-08-14 16:03:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ import os
 from shapely.geometry import Point, shape
 from flask import Flask
 from flask import render_template
+import settings
+import private
+import time
 
 app = Flask(__name__)
 
@@ -25,37 +28,6 @@ def index():
 @app.route("/data")
 def get_data():
     import json
-    engine = create_engine('postgresql://dbuser:cc0901-!@localhost:5432/gary_test')
-    df = pd.read_sql_query('select * from gary', con=engine)
-    data_path = './input/'
-    df = df.rename(columns={'coordinates': 'coord'})
-    df['latitude'] = 0
-    df['longitude'] = 0
-    df['lat_state'] = 0
-    df['long_state'] = 0
-    geolocator = Nominatim()
-    for i in df.coord.index:
-        if df.coord[i] is not None:
-            df.loc[i, 'latitude'] = json.loads(df.coord[i]).get('coordinates')[1]
-            df.loc[i, 'longitude'] = json.loads(df.coord[i]).get('coordinates')[0]
-        # else:
-        #     if df.user_location[i] is not None:
-        #         try:
-        #             #df.loc[i, 'lat_state'] = geolocator.geocode(df.user_location[i]).latitude
-        #             #df.loc[i, 'long_state'] = geolocator.geocode(df.user_location[i]).longitude
-        #
-        #             # df.loc[i, 'lat_state'] = geocoder.google(df.user_location[i]).lat
-        #             # df.loc[i, 'long_state'] = geocoder.google(df.user_location[i]).lng
-        #
-        #             print(i)
-        #         except:
-        #             print("faster...")
-    print ("Finished load latlng !")
-
-    # set 30000
-    #df = df[df['lat_state'] != 0].sample(n=100)
-    df = df[df['longitude'] != 0]
-
     def get_follower_segment(user_followers):
         if user_followers <= 100:
             return '0-100'
@@ -65,20 +37,6 @@ def get_data():
             return '1000-10,000'
         else:
             return '> 10,000'
-
-    df['phone_brand_en'] = df['user_followers'].apply(
-        lambda user_followers: get_follower_segment(user_followers))
-
-    df['gender'] = "Yes"
-    df['age_segment'] = "No"
-
-
-    from shapely.geometry import Point, shape
-    import json
-
-    with open(data_path + 'geojson/us-states.json') as data_file:
-        states_json = json.load(data_file)
-
     def get_location(longitude, latitude, states_json):
         point = Point(longitude, latitude)
         for record in states_json['features']:
@@ -87,16 +45,84 @@ def get_data():
             if polygon.contains(point):
                 return record['properties']['name']
         return 'other'
+    def get_local_data():
+        with open('./input/data.json', 'r') as data:
+            df = data.read()
+            data.close()
+        return df
+    engine = create_engine('postgresql://postgres:' + private.password + '@localhost:5432/postgres')
+    # check database exist
+    # dbname = 'gary_test'
+    dbname = private.DB_NAME
+    table = settings.TABLE_NAME
+    df = pd.read_sql_query("select * from pg_database where datname='" + dbname + "'", con=engine)
+    if df.empty is True:
+        return get_local_data()
+    else:
+        engine = create_engine('postgresql://postgres:' + private.password + '@localhost:5432/' + dbname)
+        df = pd.read_sql_query("select * from information_schema.tables where table_name='" + table + "' and table_schema='public'", con=engine)
+        if df.empty is True:
+            return get_local_data()
+        else:
+            # check table exists
+            #df = pd.read_sql_query('select * from gary limit 10', con=engine)
+            df = pd.read_sql_query('select * from ' + table + ' limit 10', con=engine)
+            if df.empty is True:
+                # read from data.json
+                return get_local_data()
+            else:
+                # run dynamic generation code
+                data_path = './input/'
+                df = df.rename(columns={'coordinates': 'coord'})
+                df['latitude'] = 0
+                df['longitude'] = 0
+                df['lat_state'] = 0
+                df['long_state'] = 0
+                geolocator = Nominatim()
+                # if df is not None:
+                for i in df.coord.index:
+                    if df.coord[i] is not None:
+                        df.loc[i, 'latitude'] = json.loads(df.coord[i]).get('coordinates')[1]
+                        df.loc[i, 'longitude'] = json.loads(df.coord[i]).get('coordinates')[0]
+                    else:
+                        if df.user_location[i] is not None:
+                            try:
+                                df.loc[i, 'latitude'] = geolocator.geocode(df.user_location[i]).latitude
+                                df.loc[i, 'longitude'] = geolocator.geocode(df.user_location[i]).longitude
+                                #
+                                # df.loc[i, 'latitude'] = geocoder.google(df.user_location[i]).lat
+                                # df.loc[i, 'longitude'] = geocoder.google(df.user_location[i]).lng
+                                # df.loc[i, 'latitude'] = Geocoder.geocode(df.user_location[i]).lat
+                                # df.loc[i, 'longitude'] = Geocoder.geocode(df.user_location[i]).lng
+                                #
+                                print(i)
+                            except:
+                                print("faster...")
+                print ("Finished decoding latlng !")
+                # else:
+                # df = df.read_csv('./input/save_gary1.csv')
+                # set 30000
+                # df = df[df['lat_state'] != 0].sample(n=100)
+                df = df[df['longitude'] != 0]
+                df['phone_brand_en'] = df['user_followers'].apply(
+                    lambda user_followers: get_follower_segment(user_followers))
+                df['gender'] = "Yes"
+                df['age_segment'] = "No"
+                from shapely.geometry import Point, shape
+                import json
+                with open(data_path + 'geojson/us-states.json') as data_file:
+                    states_json = json.load(data_file)
+                    data_file.close()
+                df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], states_json), axis=1)
+                df = df.rename(columns={'created': 'timestamp'})
+                cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment',
+                                'location','user_description','text']
+                df.to_csv('save_gary.csv', index=False, encoding='utf-8')
+                df_clean = df[cols_to_keep].dropna()
+                df_clean['timestamp'] = df_clean['timestamp'].astype(str)
+                tt = df_clean.to_json( orient='records', date_format='iso', date_unit = 's')
+                return tt
 
-    df['location'] = df.apply(lambda row: get_location(row['longitude'], row['latitude'], states_json), axis=1)
-    df = df.rename(columns={'created': 'timestamp'})
-    cols_to_keep = ['timestamp', 'longitude', 'latitude', 'phone_brand_en', 'gender', 'age_segment',
-                    'location','user_description','text']
-    df.to_csv('save_gary.csv', index=False, encoding='utf-8')
-    df_clean = df[cols_to_keep].dropna()
-    df_clean['timestamp'] = df_clean['timestamp'].astype(str)
-    tt = df_clean.to_json( orient='records', date_format='iso', date_unit = 's')
-    return tt
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5002, debug=True)
